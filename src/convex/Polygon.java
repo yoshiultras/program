16166ec8a60c4541069ec9c5e24b53502c4c5a27
yoshiultras <nikita.salivonov@yandex.ru>
2023-05-10 22:55:34 +0300

    Convex без внутренних точек

--- src/convex/Polygon.java
+++ src/convex/Polygon.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 //Класс "Многоугольник", реализующий интерфейс фигуры.
 public class Polygon extends Deq implements Figure{
     private double s, p;
-    private ArrayList<R2Point> history;
+
     public Polygon(R2Point a, R2Point b, R2Point c){
         pushFront(b);
 
@@ -22,10 +22,7 @@ public class Polygon extends Deq implements Figure{
         p = R2Point.dist(a, b) + R2Point.dist(b, c)+ R2Point.dist(c, a);
         s = Math.abs(R2Point.area(a, b, c));
 
-        history = new ArrayList<R2Point>();
-        history.add(a);
-        history.add(b);
-        history.add(c);
+
     }
 
     public double perimeter(){
@@ -42,7 +39,7 @@ public class Polygon extends Deq implements Figure{
     }
 
     public Figure add(R2Point t){
-        history.add(t);
+
         int i;
         //Ищем освещенные ребра, просматривая их одно за другим.
         for(i = length(); i>0 && !t.light(back(), front()); i--)
@@ -80,13 +77,12 @@ public class Polygon extends Deq implements Figure{
         for (int i = 0; i < length(); i++) {
             R2Point p = popBack();
             pushFront(p);
+            p.draw(g);
             x[i] = p.getX();
             y[i] = p.getY();
         }
+        g.setColor(new Color(0x0000FF));
         g.drawPolygon(x, y, length());
-        for (int i = 0; i < history.size(); i++) {
-            R2Point p = history.get(i);
-            p.draw(g);
-        }
+
     }
 }
