257d98a9e1b35216a10bde4ac266f7c1e2558908
yoshiultras <n.a@f.d>
2023-06-03 15:43:01 +0300

    stack calc v2

---
+++ src/stack/calc/StackInt.java
@@ -0,0 +1,26 @@
+package stack.calc;
+
+//Непрерывная реализация стека целых чисел.
+class StackInt{
+    private static final int DEFSIZE = 16;
+    private int[] array;
+    private int head;
+
+    public StackInt(){
+        array = new int[DEFSIZE];
+        head = 0;
+    }
+
+    public final void push(int val){
+        array[head++] = val;
+    }
+
+    public final int pop(){
+        return array[--head];
+    }
+
+    public final int top(){
+        return array[head - 1];
+    }
+
+}
